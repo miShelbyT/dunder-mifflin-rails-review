66d78a5641bb55f54b3403b797e37b913dc25857
miShelbyT <mi.shelbyrose@gmail.com>
2020-10-08 19:33:15 -0400

    lab done with Sisan

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -9,30 +9,24 @@ class EmployeesController < ApplicationController
 	end
 
 	def index
-        @employees = Employee.all 
+    @employees = Employee.all 
 	end
 		
 	def new
 		@employee = Employee.new
 	end
     
-		def create 
-        @employee = Employee.new(params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :dog_id))
-				byebug
-        @employee.save
-        redirect_to employee_path(@employee)
+	def create 
+    @employee = Employee.new(params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :dog_id))
+    @employee.save
+    redirect_to employee_path(@employee)
 	end
 		
 	def update
-		@employee = Employee.find(params[:id]).update(params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :dog_id))
-        redirect_to employee_path(@employee)
+		@employee = Employee.find(params[:id])
+		@employee.update(params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :dog_id))
+    redirect_to employee_path(@employee)
 	end
 
-	private
-		
 
-
-    # def employee_params
-    #     params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :dog_id)
-    # end
 end
